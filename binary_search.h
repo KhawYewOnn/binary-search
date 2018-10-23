f1810234c1cde31a00b7659d52eadbcc2e3a9d13
Khaw Yew Onn <khaw.yewonn@gmail.com>
2018-10-23 18:29:47 +0800

    add ability to test correctness of output from different binary search method

--- binary_search.h
+++ binary_search.h
@@ -7,4 +7,6 @@
 #include <vector>
 std::vector<int> binary_search(const std::vector<double>& targets, const std::vector<int>& sorted_array);
 std::vector<int> heap_binary_search(const std::vector<double>& targets, const std::vector<int>& sorted_array);
+std::vector<int> build_heap(int size);
+std::vector<int> cache_heap_search(const std::vector<double>& targets, const std::vector<int>& sorted, int cache_height);
 #endif /*BINARY_SEARCH_H */
---
+++ use_binary_search.cpp
@@ -0,0 +1,77 @@
+#include "binary_search.h"
+#include <vector>
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+//vector<int> binary_search(const vector<double>& targets, const vector<int>& sorted_numbers);
+//vector<int> heap_binary_search(const vector<double>& targets, const vector<int>& sorted_numbers);
+//argv[1] --> size of sorted_array to test on
+//argv[2] --> number of queries
+int main(int argc, char** argv)
+{
+    int size = 32;
+    int queries_count = 10;
+    int cache_height = 3;
+
+    if (argc >= 3 && argc <= 4) {
+        size = pow(2, stoi(argv[1]));
+        queries_count = stoi(argv[2]);
+        cache_height = (argc == 4) ? stoi(argv[3]) : 3;
+    }
+
+    cout<<"size = "<<size<<endl;
+    cout<<"num q = "<<queries_count<<endl;
+
+    vector<int> numbers;
+    vector<int> original;
+    vector<double> queries;
+    for (int i = 0 ; i < size; i++) {
+        numbers.push_back(i);
+    }
+
+    for (int i = 0; i < queries_count; i++) {
+        queries.push_back(i + 0.2);
+    }
+    /**
+      srand(time(NULL));
+      for (int i = 0; i < size; i++) {
+      int val = rand() % (size * 10);
+      numbers.push_back(val);
+      original.push_back(val);
+      }
+      sort (numbers.begin(), numbers.end());
+
+      for (int i = 0; i < queries_count; i++) {
+      int val = rand() % (size * 10 + 5);
+      queries.push_back(val);
+      }
+     **/
+    vector<int> heap_solutions = heap_binary_search(queries, numbers);
+    vector<int> normal_solutions = binary_search(queries, numbers);
+    vector<int> cache_heap_solutions = cache_heap_search(queries, numbers, cache_height);
+    bool is_same = true;
+
+    if (heap_solutions.size() != normal_solutions.size() || 
+            heap_solutions.size() != cache_heap_solutions.size()) {
+        cout<<"diffrent solution size..."<<endl;
+        is_same = false;
+    }
+
+    for (int i = 0; i < queries.size(); i++) {
+        double target = queries[i];
+        int heap = heap_solutions[i];
+        int normal = normal_solutions[i];
+        int cache_heap = cache_heap_solutions[i];
+        if (normal != heap || normal != cache_heap) { 
+            is_same = false;
+            cout<<"target = "<<target<<" , heap = "<<heap<<" , normal "<<normal
+                <<" , cache_heap = "<<cache_heap<<endl;
+        }
+        //        cout<<"target = "<<target<<" , heap = "<<heap<<" , normal "<<normal
+        //            <<" , cache_heap = "<<cache_heap<<endl;
+    }
+    cout<<"is_same = "<<is_same<<endl;
+}
